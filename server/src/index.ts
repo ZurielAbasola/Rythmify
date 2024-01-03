20cb0eaa9c7968091f8f7fb7ff47ca3ec0ac21f5
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-01-03 14:25:01 -0700

    Changes to configurations

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import exp from "constants";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post(
+  "/",
+  (req, res, next) => {},
+  (req, res) => {
+    console.log(req.body);
+    res.json({ message: "Hello, World!" });
+  }
+);
+
+app.listen(8000, () => {
+  console.log("listening");
+});
